e9e0206de32eb643b951c8f35f7a302071f94620
andri689 <andriandwi0529@gmail.com>
2023-12-19 11:55:31 +0700

    feat: conditional in js

---
+++ js--basic/fungsi/function.js
@@ -0,0 +1,22 @@
+//Pendefinisian Fungsi
+function greeting() {
+    console.log("Selamat datang");
+}
+
+//Pemanggilan Fungsi
+greeting();
+
+//Parameter dan Argumen
+function myGreeting(paran) {
+    console.log(paran);
+}
+
+myGreeting("Selamat datang di CVZN Teams");
+myGreeting("Selamat datang di Back End Teams");
+
+//Nilai Kembalian
+function myInfo(bio) {
+    return bio;
+}
+
+console.log(myInfo("Halo namaku Andrian"));
