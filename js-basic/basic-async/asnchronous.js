a0736d5ab68f047c4ca52c10b20233a69e198eb6
andri689 <andriandwi0529@gmail.com>
2023-12-27 14:01:24 +0700

    feat: basic-async & study-case

---
+++ js-basic/basic-async/asnchronous.js
@@ -0,0 +1,19 @@
+function f1() {
+    setTimeout(function() {
+        console.log("Aku asynchronous!")
+    }, 3000)
+}
+
+function f2() {
+   setTimeout(function() {
+    console.log("Panggil aku...")
+   }, 3000)
+}
+
+function f3() {
+    console.log("Aku ketiga...")
+}
+
+f1()
+f2()
+f3()
---
+++ js-basic/basic-async/study-case.js
@@ -0,0 +1,36 @@
+
+function panaskanAir() {
+    setTimeout(function() {
+        console.log("Panaskan Air...")
+    })
+}
+
+function ambilTeh() {
+    setTimeout(function() {
+        console.log("Ambil 1 Teh Celup Taruh di Gelas...")
+    })
+}
+
+function ambilGula() {
+    setTimeout(function() {
+        console.log("Beri Gula Secukupnya...")
+    })
+}
+
+function adukTeh() {
+    setTimeout(function() {
+        console.log("Aduk Teh Celup...")
+    })
+}
+
+function siap() {
+    setTimeout(function() {
+        console.log("Teh siap dinikmati!")
+    })
+}
+
+panaskanAir()
+ambilTeh()
+ambilGula()
+adukTeh()
+siap()
