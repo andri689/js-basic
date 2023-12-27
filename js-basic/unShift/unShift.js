efc1b0a26de79ae40a47d94f22cff301cad1e55d
andri689 <andriandwi0529@gmail.com>
2023-12-27 11:13:19 +0700

    feat: unShift array methods in js

---
+++ js-basic/unShift/unShift.js
@@ -0,0 +1,7 @@
+// unshift digunakan untuk menambah atau menambah element
+// Urutan array dimulai dari 0,1,2 dsb
+let myHobby = ["Play Game", "Menonton Film", "Read Novel"];
+
+//myHobby.unshift("Traveling");// Menambah Element Pertama Hasil [ 'Traveling', 'Play Game', 'Menonton Film', 'Read Novel' ]
+myHobby.unshift("Traveling","Membaca"); // Hasil [ 'Traveling', 'Membaca', 'Play Game', 'Menonton Film', 'Read Novel' ]
+console.log(myHobby);
