8c52c56c7d44abd92eb89fd7f8e676e3f65280ff
andri689 <andriandwi0529@gmail.com>
2023-12-29 16:47:38 +0700

    feat: add argumen method

---
+++ js-basic/conditional/if-else.js
@@ -0,0 +1,29 @@
+const bil = 4;
+let answer;
+
+if (bil == 9) {
+    answer = 'ini bilangan ganjil';
+    console.log('ini bilangan ganjil')
+} else if (bil == 4) {
+    answer = 'ini bilangan genap';
+    console.log('ini bilangan genap')
+} else {
+    answer = 'bukan bilangan'
+    console.log('bukan bilangan')
+}
+
+if ("kondisi") {
+    //apa yang mau dijalankan 
+} else if ("kondisi2") {
+    //apa yang mau dijalankan 
+} else {
+    //kode yang mau dijalankan
+}
+
+if (meetSchedule == 9) {
+    answer = "ini meet pagi kita";
+} else if (meetSchedule ==13) {
+    answer = "ini meet siang kita";
+} else {
+    answer = "itu bukan jadwal meet kita";
+}
---
+++ js-basic/loop/index.js
@@ -0,0 +1,15 @@
+// loop da loop 
+for(let i = 0; i <= 10; i++) {
+    console.log(i)
+}
+
+const fruits = ["Mangga", "Apel", "Jeruk", "Pepaya"];
+
+for (let i = 0; i < fruits.length; i++) {
+    console.log("Buah: " + fruits[i])
+}
+
+const makanan = ["SATE","BAKSO","MI AYAM","AYAM PANGGANG", "NASI GORENG", "MI GORENG"]
+for (let i = 0; i < makanan.length; i++){
+    console.log("Saya Suka Makan : " + makanan[i])
+}
